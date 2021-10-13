ad72b000867c0b0a963d72f1201eb09368160831
Dome1902 <dome1902@web.de>
2021-10-13 14:10:12 +0200

    feat(test): ahhhhhhhh

---
+++ test.js
@@ -0,0 +1,16 @@
+let input = "Foo";
+input = 1337;
+const price = `The price of the new phone ist ${phone} with a discount of ${discount}`;
+
+//null = empty, unbekannt
+//undefined =  wert noch nicht zugewiesen
+
+const user = {
+    name: "Dome",
+    age : 25,
+    gender: "m",
+}
+//console.log(user.name);
+
+console.log(input);
+
